ece6452b6063b9fb18c0479025aef4c3447b06c3
Felipe Prates <fdspr4tes@gmail.com>
2025-07-28 20:59:02 -0300

    "manipulação de banco sqlite com insert, update, delete"

---
+++ 03 - Gerenciamento de pacotes/01_boas_praticas.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+print(os)
+
+
+print(sys)
+a = "pythn"
+
+b = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+
+frutas = [
+    "pera",
+    "maçã",
+    "uva",
+    "abacate",
+    "ameixa",
+    "pera",
+    "melancia",
+    "laranja",
+    "melão",
+    "morango",
+    "banana",
+    "carambola",
+    "carambola,",
+]
+
+
+carros = ["ferrari", "bmw", "corsa", "palio", "gol"]
